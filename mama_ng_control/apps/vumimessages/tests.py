92511543a165e369afb08986747dd47f272729df
Mike Jones <mike@westerncapelabs.com>
2015-06-24 17:42:40 +0200

    Full CRUD tests for messages API

--- mama_ng_control/apps/vumimessages/tests.py
+++ mama_ng_control/apps/vumimessages/tests.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 from django.test import TestCase
 from django.contrib.auth.models import User
@@ -7,7 +8,7 @@ from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
 
 
-from .models import Subscription
+from .models import Inbound, Outbound
 from mama_ng_control.apps.contacts.models import Contact
 
 
@@ -33,23 +34,34 @@ class AuthenticatedAPITestCase(APITestCase):
         self.contact = contact.id
 
 
-class TestSubscriptionsAPI(AuthenticatedAPITestCase):
+class TestVumiMessagesAPI(AuthenticatedAPITestCase):
 
-    def make_subscription(self):
+    def make_outbound(self):
         post_data = {
             "contact": "/api/v1/contacts/%s/" % self.contact,
-            "messageset_id": "1",
-            "next_sequence_number": "1",
-            "lang": "en_ZA",
-            "active": "true",
-            "completed": "false",
-            "schedule": "1",
-            "process_status": "0",
-            "metadata": {
-                "source": "RapidProVoice"
-            }
+            "vumi_message_id": "075a32da-e1e4-4424-be46-1d09b71056fd",
+            "content": "Simple outbound message",
+            "delivered": "false",
+            "attempts": 1,
+            "metadata": {}
         }
-        response = self.client.post('/api/v1/subscriptions/',
+        response = self.client.post('/api/v1/messages/outbound/',
+                                    json.dumps(post_data),
+                                    content_type='application/json')
+        return response.data["id"]
+
+    def make_inbound(self, in_reply_to):
+        post_data = {
+            "message_id": str(uuid.uuid4()),
+            "in_reply_to": in_reply_to,
+            "to_addr": "+27123",
+            "from_addr": "020",
+            "content": "Call delivered",
+            "transport_name": "test_voice",
+            "transport_type": "voice",
+            "helper_metadata": {}
+        }
+        response = self.client.post('/api/v1/messages/inbound/',
                                     json.dumps(post_data),
                                     content_type='application/json')
         return response.data["id"]
@@ -57,7 +69,8 @@ class TestSubscriptionsAPI(AuthenticatedAPITestCase):
     def test_login(self):
         request = self.client.post(
             '/api-token-auth/',
-            {"username": "testuser", "password": "testpass"})
+            {"username": "testuser",
+             "password": "testpass"})
         token = request.data.get('token', None)
         self.assertIsNotNone(
             token, "Could not receive authentication token on login post.")
@@ -65,60 +78,140 @@ class TestSubscriptionsAPI(AuthenticatedAPITestCase):
                          "Status code on /auth/token/ was %s (should be 200)."
                          % request.status_code)
 
-    def test_create_subscription_data(self):
-        post_subscription = {
+    def test_create_outbound_data(self):
+        post_outbound = {
             "contact": "/api/v1/contacts/%s/" % self.contact,
-            "messageset_id": "1",
-            "next_sequence_number": "1",
-            "lang": "en_ZA",
-            "active": "true",
-            "completed": "false",
-            "schedule": "1",
-            "process_status": "0",
-            "metadata": {
-                "source": "RapidProVoice"
-            }
+            "vumi_message_id": "075a32da-e1e4-4424-be46-1d09b71056fd",
+            "content": "Say something",
+            "delivered": "false",
+            "attempts": 1,
+            "metadata": {}
         }
-        response = self.client.post('/api/v1/subscriptions/',
-                                    json.dumps(post_subscription),
+        response = self.client.post('/api/v1/messages/outbound/',
+                                    json.dumps(post_outbound),
                                     content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        d = Subscription.objects.last()
+        d = Outbound.objects.last()
         self.assertIsNotNone(d.id)
         self.assertEqual(d.version, 1)
-        self.assertEqual(d.messageset_id, 1)
-        self.assertEqual(d.next_sequence_number, 1)
-        self.assertEqual(d.lang, "en_ZA")
-        self.assertEqual(d.active, True)
-        self.assertEqual(d.completed, False)
-        self.assertEqual(d.schedule, 1)
-        self.assertEqual(d.process_status, 0)
-        self.assertEqual(d.metadata["source"], "RapidProVoice")
+        self.assertEqual(str(d.contact.id), str(self.contact))
+        self.assertEqual(d.content, "Say something")
+        self.assertEqual(d.delivered, False)
+        self.assertEqual(d.attempts, 1)
+        self.assertEqual(d.metadata, {})
 
-    def test_update_subscription_data(self):
-        existing = self.make_subscription()
-        patch_subscription = {
-            "next_sequence_number": "10",
-            "active": "false",
-            "completed": "true"
+    def test_create_outbound_data_simple(self):
+        post_outbound = {
+            "contact": "/api/v1/contacts/%s/" % self.contact,
+            "delivered": "false",
+            "metadata": {
+                "voice_speech_url": "https://foo.com/file.mp3"
+            }
         }
-        response = self.client.patch('/api/v1/subscriptions/%s/' % existing,
-                                     json.dumps(patch_subscription),
+        response = self.client.post('/api/v1/messages/outbound/',
+                                    json.dumps(post_outbound),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        d = Outbound.objects.last()
+        self.assertIsNotNone(d.id)
+        self.assertEqual(d.version, 1)
+        self.assertEqual(str(d.contact.id), str(self.contact))
+        self.assertEqual(d.delivered, False)
+        self.assertEqual(d.attempts, 0)
+        self.assertEqual(d.metadata, {
+            "voice_speech_url": "https://foo.com/file.mp3"
+        })
+
+    def test_update_outbound_data(self):
+        existing = self.make_outbound()
+        patch_outbound = {
+            "delivered": "true",
+            "attempts": 2
+        }
+        response = self.client.patch('/api/v1/messages/outbound/%s/' %
+                                     existing,
+                                     json.dumps(patch_outbound),
                                      content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        d = Subscription.objects.get(pk=existing)
-        self.assertEqual(d.active, False)
-        self.assertEqual(d.completed, True)
-        self.assertEqual(d.next_sequence_number, 10)
-        self.assertEqual(d.lang, "en_ZA")
+        d = Outbound.objects.get(pk=existing)
+        self.assertEqual(d.version, 1)
+        self.assertEqual(str(d.contact.id), str(self.contact))
+        self.assertEqual(d.delivered, True)
+        self.assertEqual(d.attempts, 2)
+        self.assertEqual(d.metadata, {})
 
-    def test_delete_subscription_data(self):
-        existing = self.make_subscription()
-        response = self.client.delete('/api/v1/subscriptions/%s/' % existing,
+    def test_delete_outbound_data(self):
+        existing = self.make_outbound()
+        response = self.client.delete('/api/v1/messages/outbound/%s/' %
+                                      existing,
                                       content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-        d = Subscription.objects.filter(id=existing).count()
+        d = Outbound.objects.filter(id=existing).count()
+        self.assertEqual(d, 0)
+
+    def test_create_inbound_data(self):
+        existing_outbound = self.make_outbound()
+        out = Outbound.objects.get(pk=existing_outbound)
+        message_id = str(uuid.uuid4())
+        post_inbound = {
+            "message_id": message_id,
+            "in_reply_to": out.vumi_message_id,
+            "to_addr": "+27123",
+            "from_addr": "020",
+            "content": "Call delivered",
+            "transport_name": "test_voice",
+            "transport_type": "voice",
+            "helper_metadata": {}
+        }
+        response = self.client.post('/api/v1/messages/inbound/',
+                                    json.dumps(post_inbound),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        d = Inbound.objects.last()
+        self.assertIsNotNone(d.id)
+        self.assertEqual(d.message_id, message_id)
+        self.assertEqual(d.to_addr, "+27123")
+        self.assertEqual(d.from_addr, "020")
+        self.assertEqual(d.content, "Call delivered")
+        self.assertEqual(d.transport_name, "test_voice")
+        self.assertEqual(d.transport_type, "voice")
+        self.assertEqual(d.helper_metadata, {})
+
+    def test_update_inbound_data(self):
+        existing_outbound = self.make_outbound()
+        out = Outbound.objects.get(pk=existing_outbound)
+        existing = self.make_inbound(out.vumi_message_id)
+
+        patch_inbound = {
+            "content": "Opt out"
+        }
+        response = self.client.patch('/api/v1/messages/inbound/%s/' %
+                                     existing,
+                                     json.dumps(patch_inbound),
+                                     content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        d = Inbound.objects.get(pk=existing)
+        self.assertEqual(d.to_addr, "+27123")
+        self.assertEqual(d.from_addr, "020")
+        self.assertEqual(d.content, "Opt out")
+        self.assertEqual(d.transport_name, "test_voice")
+        self.assertEqual(d.transport_type, "voice")
+        self.assertEqual(d.helper_metadata, {})
+
+    def test_delete_inbound_data(self):
+        existing_outbound = self.make_outbound()
+        out = Outbound.objects.get(pk=existing_outbound)
+        existing = self.make_inbound(out.vumi_message_id)
+        response = self.client.delete('/api/v1/messages/inbound/%s/' %
+                                      existing,
+                                      content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        d = Inbound.objects.filter(id=existing).count()
         self.assertEqual(d, 0)
