d6a9fa9cff38ead5ebc86a556c3593ddca473211
Mike Jones <mike@westerncapelabs.com>
2015-07-01 21:02:10 +0200

    Add helper to the contact model for getting address

--- mama_ng_control/apps/contacts/models.py
+++ mama_ng_control/apps/contacts/models.py
@@ -5,6 +5,13 @@ from django.db import models
 
 
 class Contact(models.Model):
+
+    """
+    version: 1
+    details should contain at minimum:
+    addresses: addr_type:addr_value pairs (e.g. "msisdn:+27001 msisdn:+27002 email:foo@bar.com")
+    default_addr_type: which addr_type in addresses to default to if non-given
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     version = models.IntegerField(default=1)
     details = HStoreField()
@@ -13,3 +20,21 @@ class Contact(models.Model):
 
     def __str__(self):  # __unicode__ on Python 2
         return str(self.id)
+
+    def address(self, addr_type=None):
+        """
+        returns a list of all matches or empty list
+        """
+        found = []
+        if addr_type is None and "default_addr_type" in self.details:
+            addr_type = self.details["default_addr_type"]
+        elif addr_type is None and "default_addr_type" not in self.details:
+            # fall back to sensible default
+            addr_type = "msisdn"
+        if "addresses" in self.details:
+            addresses = self.details["addresses"].split()
+            for address in addresses:
+                parts = address.split(":")
+                if parts[0] == unicode(addr_type):
+                    found.append(parts[1])
+        return found
