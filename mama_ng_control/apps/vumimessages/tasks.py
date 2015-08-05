cf6bbab0905af6021ddab0915267da5c2343a31c
Mike Jones <mike@westerncapelabs.com>
2015-08-05 14:17:16 +0200

    flake8

--- mama_ng_control/apps/vumimessages/tasks.py
+++ mama_ng_control/apps/vumimessages/tasks.py
@@ -47,8 +47,8 @@ class Send_Metric(Task):
         l.info("Firing metric: %r [%s] -> %g" % (metric, agg, float(value)))
         try:
             # TODO: Real metric firing
-            #sender = self.vumi_client()
-            #result = sender.fire_metric(metric, value, agg=agg)
+            # sender = self.vumi_client()
+            # result = sender.fire_metric(metric, value, agg=agg)
             result = {"success": "Fake metric fired"}
             l.info("Result of firing metric: %s" % (result["success"]))
             return result
