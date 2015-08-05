09c01e5e71163fc646cd7dadcdc99fa96af617a9
Mike Jones <mike@westerncapelabs.com>
2015-08-05 14:13:45 +0200

    Add search endpoint for msisdn

--- mama_ng_control/apps/contacts/views.py
+++ mama_ng_control/apps/contacts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Contact
 from .serializers import ContactSerializer
@@ -13,3 +13,20 @@ class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
     filter_fields = ('details',)
+
+
+class ContactSearchList(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ContactSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the contacts
+        for the supplied msisdn
+        """
+        contains = {
+            "addresses": "msisdn:%s" % self.request.query_params["msisdn"]
+        }
+        data = Contact.objects.filter(
+            details__contains=contains)
+        return data
